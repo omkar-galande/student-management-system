e6b3a6c9f0d4f561af7af4c786b4ece9a88eaf59
omkar-galande <galandeomkar07@gmail.com>
2026-08-03 22:30:30 +0530

    Created Student entity

--- src/main/java/com/omkar/student_management/entity/Student.java
+++ src/main/java/com/omkar/student_management/entity/Student.java
@@ -1,4 +1,23 @@
 package com.omkar.student_management.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
+@Entity
+@Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String course;
+    private String address;
+
 }
