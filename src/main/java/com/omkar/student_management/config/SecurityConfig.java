b3e4536bd4b332f8d128c5a12ab5096c250a1fa8
omkar-galande <galandeomkar07@gmail.com>
2026-09-13 17:35:43 +0530

    Add multiple users and role based authorization

--- src/main/java/com/omkar/student_management/config/SecurityConfig.java
+++ src/main/java/com/omkar/student_management/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +20,27 @@ public class SecurityConfig {
                 .httpBasic(httpBasic -> {})
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/students/health").permitAll()
-                        .requestMatchers(HttpMethod.DELETE,"/students/**").hasRole()
+                        .requestMatchers(HttpMethod.DELETE,"/students/**").hasRole("ADMIN")
                         .anyRequest().authenticated());
         return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{noop}admin@123")
+                .roles("ADMIN")
+                .build();
+
+        UserDetails student = User.builder()
+                .username("student")
+                .password("{noop}student@123")
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, student);
+
+    }
+
 }
