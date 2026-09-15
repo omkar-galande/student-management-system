a5bf26ee0db6e94a69f334a60f36d08c46bf88d3
omkar-galande <galandeomkar07@gmail.com>
2026-09-15 23:02:03 +0530

    Add database user authentication service

---
+++ src/main/java/com/omkar/student_management/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.omkar.student_management.service;
+
+import com.omkar.student_management.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        com.omkar.student_management.entity.User user =
+                userRepository.findByUsername(username)
+                        .orElseThrow(() -> new RuntimeException("User not found"));
+         return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+
+}
