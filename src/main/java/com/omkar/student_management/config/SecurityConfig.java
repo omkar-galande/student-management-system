140c54c88296930a098ed0d953144712a84d700b
omkar-galande <galandeomkar07@gmail.com>
2026-09-19 23:06:25 +0530

    Implement database user authentication

--- src/main/java/com/omkar/student_management/config/SecurityConfig.java
+++ src/main/java/com/omkar/student_management/config/SecurityConfig.java
@@ -1,16 +1,14 @@
 package com.omkar.student_management.config;
 
+import com.omkar.student_management.entity.User;
+import com.omkar.student_management.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.authentication.PasswordEncoderParser;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -28,28 +26,27 @@ public class SecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder){
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(passwordEncoder.encode("admin@123"))
-                .roles("ADMIN")
-                .build();
 
-
-        UserDetails student = User.builder()
-                .username("student")
-                .password(passwordEncoder.encode("student@123"))
-                .roles("STUDENT")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, student);
-
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+
+    @Bean
+    public CommandLineRunner createAdmin(UserRepository userRepository,
+                                         PasswordEncoder passwordEncoder) {
+        return args -> {
+            User user = new User();
+            user.setUsername("admin");
+            user.setPassword(passwordEncoder.encode("admin@123"));
+            user.setRole("ADMIN");
+
+            userRepository.save(user);
+        };
+
+    }
+    
 
 }
--- src/main/java/com/omkar/student_management/service/CustomUserDetailsService.java
+++ src/main/java/com/omkar/student_management/service/CustomUserDetailsService.java
@@ -3,7 +3,9 @@ package com.omkar.student_management.service;
 import com.omkar.student_management.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
